eefb010eb3de9ba10cc1363734d4cd5df4bf30fc
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2024-09-05 13:46:28 +0200

    Added test to check workflow

---
+++ RaceFlowAPI.Tests/Controllers/RacesControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceFlowAPI.Controllers;
+
+namespace RaceFlowAPI.Tests.Controllers;
+
+[TestClass]
+public class RacesControllerTest
+{
+
+    [TestMethod]
+    public void Test()
+    {
+        Assert.AreEqual("test", "test");
+    }
+}
